a8a48869c73ab494ad3daf26ea216a2c63a61bc2
Saed Daoud <eng.saed.daoud@gmail.com>
2020-10-04 09:50:30 -0400

    Implemented Header, Footer, and HomeScreen components

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className='rating'>
+      {[1, 2, 3, 4, 5].map((index) => (
+        <span key={index}>
+          <i
+            style={{ color }}
+            className={
+              value >= index
+                ? 'fas fa-star'
+                : value >= index - 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        </span>
+      ))}
+
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: '#cccc00', //'#f8e825',
+};
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string,
+  color: PropTypes.string,
+};
+
+export default Rating;
