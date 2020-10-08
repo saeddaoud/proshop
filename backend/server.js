f6e45e4f8e971ba1089b796fd1983f7b9a91fba6
Saed Daoud <eng.saed.daoud@gmail.com>
2020-10-08 05:57:52 -0400

    created a MongoDB database in MongoDB Atlas and connected it to MongoDB Compass and to the app

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.get('/', (req, res) => res.send('API is running ...'));
 app.get('/api/products', (req, res) => {
@@ -18,6 +23,7 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}!`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow
+      .bold
   );
 });
