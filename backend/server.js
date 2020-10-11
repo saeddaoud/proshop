c4b1cb682ee31ebeab3277be81f01977e343cfaa
Saed Daoud <eng.saed.daoud@gmail.com>
2020-10-11 07:28:57 -0400

    created custom error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -14,6 +15,10 @@ connectDB();
 // app.get('/', (req, res) => res.send('API is running ...'));
 
 app.use('/api/products', productRoutes);
+
+// custom error handling middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => {
