8400158737ab4069ec20c22e73d4cab54c3d90d9
Saed Daoud <eng.saed.daoud@gmail.com>
2020-10-22 06:10:55 -0400

    implemented update profile in the backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,4 +76,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile };
+//@route        GET /api/users/profile
+//@description  Get user's profile
+//@access       Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  // res.send('success');
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.password = req.body.password || user.password;
+    // if (req.body.password) {
+    //   user.password = req.body.password;
+    // }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser.id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmis: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not Found');
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
