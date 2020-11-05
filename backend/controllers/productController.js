e63fe5a08f5ee3a775ba5b4bd1b01d513c310db8
Saed Daoud <eng.saed.daoud@gmail.com>
2020-11-05 12:06:33 -0500

    created the delete product endpoint at the backend and the delete product functionality at the frontend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,4 +22,18 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProductById, getProducts };
+//@route        DELETE /api/products/:id
+//@description  Delete product by id
+//@access       Private/Amin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product has been removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProductById, getProducts, deleteProduct };
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,7 @@
 import {
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
@@ -32,6 +35,20 @@ export const productDetailsReducer = (
     case PRODUCT_DETAILS_SUCCESS:
       return { product: payload, loading: false };
     case PRODUCT_DETAILS_FAIL:
+      return { error: payload, loading: false };
+    default:
+      return state;
+  }
+};
+
+export const productDeleteReducer = (state = {}, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case PRODUCT_DELETE_REQUEST:
+      return { loading: true };
+    case PRODUCT_DELETE_SUCCESS:
+      return { success: true, loading: false };
+    case PRODUCT_DELETE_FAIL:
       return { error: payload, loading: false };
     default:
       return state;
