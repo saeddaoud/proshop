9739c7c24d802f38d09e7c97b7e69384df074063
Saed Daoud <eng.saed.daoud@gmail.com>
2020-11-10 10:55:46 -0500

    Created the create product review endpoint in the backend, and implemented the create product review in the frontend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -88,10 +88,49 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+//@route        POST /api/products/:id/reviews
+//@description  Create a product review
+//@access       Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Already Reviewed');
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, cur) => acc + cur.rating, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201);
+    res.json({ message: 'Review Added' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
 export {
   getProductById,
   getProducts,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,10 @@ import {
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_RESET,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_RESET,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_DELETE_FAIL,
   PRODUCT_DELETE_REQUEST,
@@ -90,6 +94,22 @@ export const productUpdateReducer = (state = { product: {} }, action) => {
       return { error: payload, loading: false };
     case PRODUCT_UPDATE_RESET:
       return { product: {} };
+    default:
+      return state;
+  }
+};
+
+export const productReviewCreateReducer = (state = {}, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return { loading: true };
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return { success: true, loading: false, product: payload };
+    case PRODUCT_CREATE_REVIEW_FAIL:
+      return { error: payload, loading: false };
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return {};
     default:
       return state;
   }
